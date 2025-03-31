5ca66488c40f087cc0130322c269deb9846e97e6
thiago morgado <thiago_morgado@hotmail.com>
2025-03-31 17:35:53 -0300

    Inicial commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import Home from './src/screens/main';
 
 export default function App() {
--- src/screens/main/styles.ts
+++ src/screens/main/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components/native";
+import { colors } from "../../themes/theme";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: red;
 `;
